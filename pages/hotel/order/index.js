91aed644468829ab082c0501033f8bf9d933ed56
Joeyang <1665074044@qq.com>
2017-09-18 15:13:02 +0800

    Joeyang  add

--- pages/hotel/order/index.js
+++ pages/hotel/order/index.js
@@ -13,7 +13,6 @@ Page({
     var that = this;
     var price = that.data.price;
     var result = price - that.data.deductible;
-    console.log(result)
     if (e.detail.value){
       that.setData({
         active: 'active',
@@ -32,18 +31,73 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
+    that.setData({
+      date:options.date,
+      dateEnd:options.dateEnd,
+      nightNum:options.nightNum
+    });
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        var userInfo = res.userInfo
+        var nickName = userInfo.nickName
+        var avatarUrl = userInfo.avatarUrl
+        var gender = userInfo.gender //性别 0：未知、1：男、2：女
+        var province = userInfo.province
+        var city = userInfo.city
+        var country = userInfo.country
+
+        that.setData({
+          userName: nickName
+        });
+       }
+    })
+    wx.getStorage({
+      key: 'singleHotel',
+      success: function(res) {
+        var hotel=res.data;
+        that.setData({
+          address:hotel.data.address,
+          hotelName:hotel.name,
+          phone:hotel.data.phone,
+          id:hotel.data.id
+        })
+      },
+    })
+    // wx.getStorage({
+    //   key: 'singleHotel',
+    //   success: function (res) {
+    //     var hotel = res.data;
+    //   }  
     var integral = that.data.integral;//拥有积分
     var deductible=integral/10;//可抵扣的金额
     wx.getStorage({
-      key: 'price',
+      key: 'spec',
       success: function(res) {
         that.setData({
-          price: res.data,
+          price: res.data.price,
           deductible: deductible,
-          result:res.data
+          result:res.data.price
         });
       },
     });
+  },
+
+  submit:function(){
+    var that=this;
+    var address=that.data.address;
+    var check_in = that.data.date;
+    var check_out = that.data.dateEnd;
+    var hotel_name = that.data.hotelName;
+    var price = that.data.result;
+    var user_name = that.data.userName;
+    var integral = that.data.integral;
+    var hotel_id = that.data.id;
+    var user_phone='18787312252'
+    wx.setStorage({
+      key: 'orderList',
+      data: {user_phone, address, hotel_name, check_in, check_out, price, user_name, integral, hotel_id},
+    })
   },
 
   /**
