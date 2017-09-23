f17ca3c6ff26cbac45f020b76e0e620a71fd4f08
JagoYe <yejain122@qq.com>
2017-09-23 10:05:07 +0800

    Jago app.js modified

--- app.js
+++ app.js
@@ -2,36 +2,43 @@
 App({
   onLaunch: function() {
     var that = this;
-    //登录
-    // wx.login({
-    //   success: function(res) {
-    //     if(res.code) {
-    //       //登录成功
-    //       wx.request({
-    //         url: that.globalData.webSite + '/Home/Wechat/userLogin',
-    //         data: {
-    //           code: res.code
-    //         },
-    //         success: function(res) {
-    //           //全局添加openId seesionKey
-    //           var data = JSON.parse(res.data);
-    //           that.globalData.openId = data.openid;
-    //           that.globalData.sessionKey = data.session_key;
+    wx.request({
+      url: 'http://www.jago.com:8090/jiayoulong/Home/Wechat/hotelSelectAll',
+      success: function(res) {
+        console.log(res);
+      }
+    })
+    // 登录
+    wx.login({
+      success: function(res) {
+        if(res.code) {
+          //登录成功
+          wx.request({
+            url: that.globalData.webSite + '/Home/Wechat/userLogin',
+            data: {
+              code: res.code
+            },
+            success: function(res) {
+              //全局添加openId seesionKey
+              var data = JSON.parse(res.data);
+              that.globalData.openId = data.openid;
+              that.globalData.sessionKey = data.session_key;
+              console.log(res);
 
-    //           //全局添加userInfo
-    //           wx.getUserInfo({
-    //             success: function(res) {
-    //               that.globalData.userInfo = res.userInfo;
-    //             }
-    //           });
-    //         }
-    //       })
+              //全局添加userInfo
+              wx.getUserInfo({
+                success: function(res) {
+                  that.globalData.userInfo = res.userInfo;
+                }
+              });
+            }
+          })
 
-    //     }else {
-    //       console.log('获取用户登录态失败！' + res.errMsg)
-    //     }
-    //   }
-    // });
+        }else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
   },
 
   globalData: {
