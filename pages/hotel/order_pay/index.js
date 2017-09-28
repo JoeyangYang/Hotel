3bfbfeaf1f64a7ef4c29597c05b1941ad4062241
JagoYe <yejain122@qq.com>
2017-09-28 11:14:29 +0800

    Jago modified wechat pay

--- pages/hotel/order_pay/index.js
+++ pages/hotel/order_pay/index.js
@@ -51,18 +51,28 @@ Page({
               var spec = spec.data.spec;
               var detail = member + ',' + spec;
 
+              //微信支付-----------------------------------------------------------------------------
+              //微信支付所需参数
+              var mch_id = '1488832592';  //商户号
+              var mch_key = 'G1524ghj861473f42h5s7211cr5FG261';   //商户号密钥
+              var body = '云南悦途酒店管理有限公司-嘉优隆酒店预定';  //商家名称-销售商品类目
+              var total_fee = orderList.data.price * 100;   //总价(单位为分!)
+              var spbill_create_ip = '127.0.0.1';   //支付提交用户端IP(随便写)
+              var trade_type = 'JSAPI';   //交易类型(小程序用JSAPI)
+
+
               //微信支付参数生成方法---------------------
               var payArr = {
                 appid: app.globalData.appid,
-                mch_id: '1488832592',
+                mch_id: mch_id,
                 nonce_str: app.globalData.randomString(),
-                body: '云南悦途酒店管理有限公司-嘉优隆酒店预定',
+                body: body,
                 openid: app.globalData.openId,
                 out_trade_no: app.globalData.outTradeNo(),
-                total_fee: orderList.data.price * 100,
-                spbill_create_ip: '127.0.0.1',
+                total_fee: total_fee,
+                spbill_create_ip: spbill_create_ip,
                 notify_url: app.globalData.webSite + '/Home/Admin/wecahtPayBack',
-                trade_type: 'JSAPI'
+                trade_type: trade_type
               }
               //生成sign (签名)-------------------------
               var sign = 'appid=' + payArr.appid +
@@ -75,7 +85,7 @@ Page({
                 '&spbill_create_ip=' + payArr.spbill_create_ip +
                 '&total_fee=' + payArr.total_fee +
                 '&trade_type=' + payArr.trade_type +
-                '&key=' + 'G1524ghj861473f42h5s7211cr5FG261';
+                '&key=' + mch_key;
 
 
               //统一下单--------------------------------
@@ -140,7 +150,7 @@ Page({
                         '&package=' + wechatPayArr.package +
                         '&signType=' + wechatPayArr.signType +
                         '&timeStamp=' + wechatPayArr.timeStamp +
-                        '&key=' + 'G1524ghj861473f42h5s7211cr5FG261';
+                        '&key=' + mch_key;
 
                       //后台加密
                       wx.request({
@@ -157,7 +167,6 @@ Page({
                             signType: wechatPayArr.signType,
                             paySign: wechatPayArr.paySign,
                             success: function (pay) {
-                              console.log(pay);
                               //支付成功后------------------------------------
                               wx.request({
                                 header: {
